a6a858ea533c0e798f7175e20f3f0dfe3ad050b8
van8878 <van8878@gmail.com>
2021-10-03 23:11:59 +0800

    web-update1

---
+++ script17.js
@@ -0,0 +1,18 @@
+// 部落格
+
+var title = document.getElementById("title");
+var content = document.getElementById("content");
+var btn = document.getElementById("btn");
+var list = document.getElementById("list");
+
+btn.addEventListener("click", function(){
+    list.innerHTML = list.innerHTML + `
+    <div class="article">
+        <h2>${title.value}</h2>
+        <p>${content.value}</p>
+    </div>
+    <hr/>
+    `;
+    title.value = "";
+    content.value = "";
+})
